e4b68cf7d29b85161b8d61588b6fd250fb3ab09d
kumneung <sing3demons@live.com>
2021-06-29 21:42:41 +0700

    crud TodoItems

--- Controllers/TodoItems.cs
+++ Controllers/TodoItems.cs
@@ -2,6 +2,7 @@ using System.Collections;
 namespace TodoApi.Controllers
 {
     using System.Collections.Generic;
+    using System.Linq;
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.EntityFrameworkCore;
@@ -17,6 +18,12 @@ namespace TodoApi.Controllers
         public TodoItems(DatabaseContext databaseContext)
         {
             this.databaseContext = databaseContext;
+
+            if (databaseContext.TodoItems.Count() == 0)
+            {
+                databaseContext.TodoItems.Add(new TodoItem { Name = "Learn VBA" });
+                databaseContext.SaveChanges();
+            }
         }
 
         [HttpGet]
@@ -24,7 +31,19 @@ namespace TodoApi.Controllers
         {
             var result = await databaseContext.TodoItems.ToListAsync();
 
-            return Ok(new { todoItems = result});
+            return Ok(new { todoItems = result });
+        }
+
+        //GET: api/v1/todoItems/2
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TodoItems>> GetTodoItem(long id)
+        {
+            var todoItem = await databaseContext.TodoItems.FindAsync(id);
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+            return Ok(new { todoItem = todoItem });
         }
 
         [HttpPost]
@@ -34,6 +53,28 @@ namespace TodoApi.Controllers
             await databaseContext.SaveChangesAsync();
             return CreatedAtAction("GetTodoItem", new { id = todoItem.Id }, todoItem);
             // return CreatedAtAction(nameof(GetTodoItem), new { id = todoItem.Id }, todoItem);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTodoItem(long id, TodoItem todoItem)
+        {
+            if (id != todoItem.Id) return BadRequest();
+
+            databaseContext.Entry(todoItem).State = EntityState.Modified;
+
+            await databaseContext.SaveChangesAsync();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTodaItem(long id)
+        {
+            var todoItem = await databaseContext.TodoItems.FindAsync(id);
+            if(todoItem==null)return NotFound();
+
+            databaseContext.TodoItems.Remove(todoItem);
+            await databaseContext.SaveChangesAsync();
+            return NoContent();
         }
     }
 }
--- README.md
+++ README.md
@@ -4,3 +4,8 @@ dotnet dev-certs https --trust
 dotnet add package Microsoft.EntityFrameworkCore
 dotnet add package Microsoft.EntityFrameworkCore.SqlServer
 dotnet add package Microsoft.EntityFrameworkCore.Design
+
+
+https://github.com/dotnet/AspNetCore.Docs/blob/main/aspnetcore/tutorials/first-web-api/samples/2.2/TodoApi/Controllers/TodoController.cs
+
+https://docs.microsoft.com/th-th/aspnet/core/tutorials/first-web-api?view=aspnetcore-5.0&tabs=visual-studio-code
